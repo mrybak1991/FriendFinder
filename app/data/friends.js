ac8adb6c1ef16d5ad024e4c9988c0d051a6dcc6c
matthew rybak <mrybak1991@gmail.com>
2019-08-16 11:09:38 -0400

    firstcommit

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+    {
+          name: 'Marty',
+          photo: 'http://www.hockeyworldblog.com/wp-content/uploads/2014/12/free-agent-goaltender-martin-brodeur.jpg',
+          scores: [
+              '1',
+              '5',
+              '5',
+              '1',
+              '5',
+              '5',
+              '1',
+              '1',
+              '1',
+              '5'
+          ]
+      },
+      {
+          name: 'Scotty',
+          photo: 'https://sports.cbsimg.net/images//visual/whatshot/ScottStevens041612.jpg',
+          scores: [
+              '2',
+              '4',
+              '1',
+              '1',
+              '1',
+              '1',
+              '1',
+              '2',
+              '1',
+              '5'
+          ]
+      }    
+  ];
+  
+    module.exports = friends;
