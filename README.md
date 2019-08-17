94300b897dd366706a0a5f08ff05f86df71e5e22
matthew rybak <mrybak1991@gmail.com>
2019-08-17 11:57:04 -0400

    finalcommit2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FriendFinder
+
+Friend Finder App: 
+
+This application allows you to answer a bunch of personality questions and based off your responses the app will match you with your soulmate. 
+
+The app uses the npm package express to handling routing. 
+
+Bootstrap was used for the design. 
+
+The app is deployed on Heroku. 
--- app/data/friends.js
+++ app/data/friends.js
@@ -4,13 +4,13 @@ var friends = [
           photo: 'http://www.hockeyworldblog.com/wp-content/uploads/2014/12/free-agent-goaltender-martin-brodeur.jpg',
           scores: [
               '1',
+              '4',
               '5',
+              '3',
               '5',
+              '4',
               '1',
-              '5',
-              '5',
-              '1',
-              '1',
+              '4',
               '1',
               '5'
           ]
@@ -19,18 +19,35 @@ var friends = [
           name: 'Scotty',
           photo: 'https://sports.cbsimg.net/images//visual/whatshot/ScottStevens041612.jpg',
           scores: [
-              '2',
+              '1',
               '4',
               '1',
               '1',
+              '2',
               '1',
-              '1',
-              '1',
+              '3',
               '2',
               '1',
               '5'
           ]
-      }    
+      },
+      
+      {
+        name: 'Hallsy',
+        photo: 'https://specials-images.forbesimg.com/imageserve/5be4813ca7ea4370591679a0/416x416.jpg?background=000000&cropX1=0&cropX2=408&cropY1=3&cropY2=411',
+        scores: [
+            '1',
+            '3',
+            '1',
+            '5',
+            '1',
+            '1',
+            '2',
+            '2',
+            '3',
+            '5'
+        ]
+    }
   ];
   
     module.exports = friends;
